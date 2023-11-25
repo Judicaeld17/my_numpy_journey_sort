bba5bf6cc5e09f24d4698fbbba0f2b41fac9076c
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-11-25 18:38:10 +0100

    first commit

---
+++ my_numpy_journey_sort.py
@@ -0,0 +1,4 @@
+#TASK: Create a function which take a standard array as parameter and returns a Numpy Array sorted.
+import numpy as np 
+def my_numpy_journey_sort(array):
+    return np.sort(array)
